6cab0a73454d440a085904df17186095cd4123ba
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-01-09 17:19:06 +0800

    Correction for : hex to rgb and rgb to hex

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 console.log('cli-css');
 
 //Create a function that converts from hex colors to rgb colors.
-const hexToRgb = hex =>
-  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-    .substring(1).match(/.{2}/g)
-    .map(x => parseInt(x, 16))
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
 
-console.log( `rgb (${hexToRgb(process.argv[3])})` );
+console.log(hexToRgb(process.argv[3]));
+
 
 // Create the functionality to put in 3 number values and get out the hex color.
-const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => x.toString(16).padStart(2, '0')).join('')
 
-console.log(rgbToHex(255, 255, 255));
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
 
-// Change the program to take the kind of conversion you want to do.
-
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+let r = parseInt(process.argv[3]);
+let g = parseInt(process.argv[4]);
+let b = parseInt(process.argv[5]);
+console.log(r);
+console.log(g);
+console.log(b);
+console.log ( rgbToHex(r,g,b) );
